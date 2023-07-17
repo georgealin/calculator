a261403edecc307811f5461f4ba2570150a01f94
georgealin <georgeepuran@gmail.com>
2023-07-17 14:36:36 +0300

    Create operate function

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ function divide (a,b) {
     return result;
 };
 
+let firstNumber, secondNumber, operator;
+
+function operate (operator, firstNumber, secondNumber) {
+    if (operator === '+') {
+        return add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        return substract(firstNumber, secondNumber);
+    } else if (operator === '*') {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
+    }
+};
+
