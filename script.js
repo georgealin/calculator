ac705425ebd082425d0bd6490b7b1ab32c35f470
georgealin <georgeepuran@gmail.com>
2023-07-17 14:17:22 +0300

    Create substract function

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function add (a,b) {
-    return result = a + b;
+    let result = a + b;
+    return result;
 };
-console.log(add(7,8));
+
+function substract (a,b) {
+    let result = a - b;
+    return result;
+};
+
